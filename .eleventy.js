40fc867aabe1a1b67f67c6b5e1d265dd52130289
sly <963fca2a-b913-44f9-ab90-ef923202ad4d@simplelogin.com>
2025-03-18 03:53:24 -0500

    NO COMMIT MESSAGE I'M OUT ON PERMANENT HOLIDAY FUCK OFF -ivy

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
-module.exports = function(eleventyConfig) {
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function (eleventyConfig) {
   const assetsToPass = [
     "src/assets/css",
     "src/assets/fonts",
@@ -12,11 +15,11 @@ module.exports = function(eleventyConfig) {
     "src/sven"
   ];
 
-  assetsToPass.forEach(dir => {
+  assetsToPass.forEach((dir) => {
     eleventyConfig.addPassthroughCopy(dir);
   });
 
-  eleventyConfig.addTransform("htmlmin", async function(content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", async function (content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
       try {
         return await require("html-minifier-terser").minify(content, {
@@ -32,8 +35,37 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  eleventyConfig.on("afterBuild", () => {
+    const outputDir = "_site";
+
+    function traverseDirectory(dir) {
+      fs.readdirSync(dir).forEach((file) => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+          traverseDirectory(filePath);
+        } else if (filePath.endsWith(".css")) {
+          try {
+            const cssContent = fs.readFileSync(filePath, "utf8");
+            const minified = require("esbuild").transformSync(cssContent, {
+              loader: "css",
+              minify: true
+            });
+            fs.writeFileSync(filePath, minified.code, "utf8");
+            console.log(`Minified CSS: ${filePath}`);
+          } catch (error) {
+            console.error(`Error minifying ${filePath}:`, error);
+          }
+        }
+      });
+    }
+
+    traverseDirectory(outputDir);
+  });
+
   return {
-    templateFormats: ["njk", "liquid", "md"], // exclude "html"
+    templateFormats: ["njk", "liquid", "md"],
     dir: {
       input: "src",
       output: "_site",
